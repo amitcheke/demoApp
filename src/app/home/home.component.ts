2e361db5cdc44cc4a4582c9ee618ceef72bd7a93
Amit Cheke <amitcheke03@gmail.com>
2018-09-30 21:46:25 +0530

    change request changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit,ViewChild,ElementRef  } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   
-industriesArray = ['Oil & Gas – Energy','Chemical Industry','Packaging industry','Mining Industry','Paper Industry','Construction','Utilities ','Agricultural & farm Industry','Industrial Machinery','Aerospace & Defense','Automobile components','Healthcare Equipment','Electrical components & Equipment',
-'Heavy Industrial & Electrical Equipment','Household & Commercial Appliances','Telecommunication'];
+industriesArray = ['Oil & Gas – Energy','Chemical Industry','Packaging industry','Mining Industry','Paper Industry','Construction','Utilities ','Agricultural & farm Industry','Industrial Machinery','Aerospace','Automobile components','Healthcare Equipment','Electrical components & Equipment',
+'Heavy Industrial & Electrical Equipment','Household & Commercial Appliances','Telecommunication', 'Defence', 'Lighting'];
 
 serviceArray = ["Designing (CAD/CAE)",
 "Manufacturing",
@@ -24,7 +24,7 @@ serviceArray = ["Designing (CAD/CAE)",
 "Proprietary sales",
 "3d scanning & printing"];
 
-industriesImageArray = ['gas_oil','checmical_industry','package','mining','paper','construction','tools','farming','machine','arospace_defence','automobile','medical','electrical_component','power','clean','telephone']
+industriesImageArray = ['gas_oil','checmical_industry','package','mining','paper','construction','tools','farming','machine','arospace_defence','automobile','medical','electrical_component','power','clean','telephone', 'defence', 'lighting']
 
 color = ['#f1c40f','#e67e22','#d35400','#f39c12','#1abc9c','#16a085','#2ecc71','#27ae60','#ecf0f1','#95a5a6','#7f8c8d','#bdc3c7'];
 
